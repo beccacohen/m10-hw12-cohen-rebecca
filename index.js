385214da2ca5d463fefeae73883676c606a17eee
Rebecca Cohen <rcohen2@ufl.edu>
2022-04-17 11:22:53 -0400

    performMaintenance method added

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ class Car {
   honk() {
     console.log("BEEP BEEP!")
   }
+
+  performMaintenance(){
+
+    setTimeout(performMaintenance, 3000)
+
+    function performMaintenance() {
+      console.log('maintenance complete')
+    }
+  }
 }
+
 
 const mySweetRide = new Car ('Pontiac', 'Fiero', 1988)
